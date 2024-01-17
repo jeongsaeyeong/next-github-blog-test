548582d23321e1050b0d86c10397601861d453c9
jeongsaeyeong <ooooo0516@naver.com>
2024-01-17 11:57:25 +0900

    m

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
+    basePath: isProd ? '/next_github_blog' : '',
     output: 'export',
     distDir: 'dist',
     images: {
-        unoptimized: true
+        unoptimized: true,
     }
 }
-
 module.exports = nextConfig
